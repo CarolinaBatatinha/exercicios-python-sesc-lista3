c3f27e40589eb88eb1bf39d356d089d1213a5c0b
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-10-05 09:03:46 -0300

    Melhoria do código

--- ex006.py
+++ ex006.py
@@ -5,4 +5,5 @@ for num in range(0, 100):
     
     if num % 2 != 0 and num > 0 and num < 100:
         soma += num
+        
 print(f'A soma dos números ímpares compreendidos entre 0 e 100 é igual a {soma}.')
